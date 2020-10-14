42fd3f514ce22907dd61d2f2091b1cf2d40e4e95
fyang1024 <fyang1024@gmail.com>
2020-10-14 21:39:18 +1100

    Fix lint issue

--- src/components/TxButton/index.js
+++ src/components/TxButton/index.js
@@ -153,13 +153,15 @@ function TxButton({
   const transformParams = (paramFields, inputParams, opts = { emptyAsNull: true }) => {
     // if `opts.emptyAsNull` is true, empty param value will be added to res as `null`.
     //   Otherwise, it will not be added
-    const paramVal = inputParams.map((inputParam) =>
-      typeof inputParam === 'object'
-        ? inputParam.value.trim()
-        : typeof inputParam === 'string'
-        ? inputParam.trim()
-        : inputParam
-    )
+    const paramVal = inputParams.map((inputParam) => {
+      if (inputParam === 'object') {
+        return inputParam.value.trim()
+      }
+      if (typeof inputParam === 'string') {
+        return inputParam.trim()
+      }
+      return inputParam
+    })
     const params = paramFields.map((field, ind) => ({ ...field, value: paramVal[ind] || null }))
 
     return params.reduce((memo, { type = 'string', value }) => {
