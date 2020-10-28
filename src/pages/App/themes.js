ea93ddf724b4d3fc8c38d9da3a746dc21adb7734
fyang1024 <fyang1024@gmail.com>
2020-10-28 23:35:28 +1100

    Convert Panel to a styled component which changes with the theme

--- src/pages/App/themes.js
+++ src/pages/App/themes.js
@@ -2,10 +2,15 @@ export const lightTheme = {
   backgroundColor: 'rgba(250,250,255,1)',
   backgroundImage: 'radial-gradient(circle at top, rgba(140, 121, 255, 1), rgba(250,250,255,1))',
   textColor: '#2D00F7CC',
+  panelBackground: 'linear-gradient(#fff, #fff) padding-box, linear-gradient(to right, #2d01f7, #7401b8) border-box',
+  panelBoxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
 }
 
 export const darkTheme = {
   backgroundColor: 'rgba(10,0,35,1)',
   backgroundImage: 'radial-gradient(circle at top, rgba(35, 1, 69, 1), rgba(10,0,35,1))',
   textColor: '#C0FDFFCC',
+  panelBackground:
+    'linear-gradient(#070020, #070020) padding-box, linear-gradient(to right, #bbf0ff, #9381ff) border-box',
+  panelBoxShadow: '0 4px 8px 0 rgba(0, 0, 0, 1), 0 6px 20px 0 rgba(0, 0, 0, 1)',
 }
