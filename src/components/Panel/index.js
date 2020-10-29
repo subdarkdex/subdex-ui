0ffdf78ec10cccdaebfb5ef4c202459b61b62ddc
Fei Yang <fyang1024@gmail.com>
2020-10-29 18:17:38 +1100

    Convert Panel to a styled component which changes with the theme (#24)

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -1,6 +1,16 @@
-import React from 'react'
-import './panel.css'
+import styled from 'styled-components'
 
-export default function Panel(props) {
-  return <div className="panel">{props.children}</div>
-}
+const Panel = styled.div`
+  width: 450px;
+  height: 550px;
+  padding: 50px 50px;
+  border-radius: 1.2em;
+  border: 0.2em solid transparent;
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  box-shadow: ${({ theme }) => theme.panelBoxShadow};
+  background: ${({ theme }) => theme.panelBackground};
+`
+
+export default Panel
